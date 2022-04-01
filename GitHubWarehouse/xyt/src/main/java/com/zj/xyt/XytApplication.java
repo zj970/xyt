5d569817d3aaf2e4095e3b4a956665c52a46e331
zj970 <3060529292@qq.com>
2022-04-01 22:07:56 +0800

    毕业设计-校园通-2022-4-1

--- GitHubWarehouse/xyt/src/main/java/com/zj/xyt/XytApplication.java
+++ GitHubWarehouse/xyt/src/main/java/com/zj/xyt/XytApplication.java
@@ -1,10 +1,11 @@
 package com.zj.xyt;
 
-import com.zj.xyt.Entity.Department;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.zj.xyt.Mapper")
 public class XytApplication {
 
     public static void main(String[] args) {
