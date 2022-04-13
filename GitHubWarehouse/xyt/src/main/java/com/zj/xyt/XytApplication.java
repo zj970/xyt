d57408f22636c33d7bdb780b19b66987ee620a9f
zj970 <3060529292@qq.com>
2022-04-13 21:18:30 +0800

    毕业设计-校园通-2022-4-12

--- GitHubWarehouse/xyt/src/main/java/com/zj/xyt/XytApplication.java
+++ GitHubWarehouse/xyt/src/main/java/com/zj/xyt/XytApplication.java
@@ -3,23 +3,21 @@ package com.zj.xyt;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.zj.xyt.Mapper")
-public class XytApplication {
+public class XytApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(XytApplication.class);
+    }
 
     public static void main(String[] args) {
-        try {
             SpringApplication.run(XytApplication.class, args);
-        }catch (Exception e){
-            System.out.println("报错原因");
-            e.printStackTrace();
-            log.println(e);
-            System.out.println("报错原因");
-        }
-
     }
+
 
 }
