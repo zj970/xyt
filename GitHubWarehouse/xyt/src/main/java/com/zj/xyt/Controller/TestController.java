f29770dd71c7188d52ec6f19d76ee4a00fefe0b3
zj970 <3060529292@qq.com>
2022-04-17 21:46:49 +0800

    毕业设计-校园通-2022-4-16

---
+++ GitHubWarehouse/xyt/src/main/java/com/zj/xyt/Controller/TestController.java
@@ -0,0 +1,14 @@
+package com.zj.xyt.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+    @RequestMapping("/homePage")
+    public ModelAndView homePage(){
+        ModelAndView mv = new ModelAndView("system/home/homePage");
+        return mv;
+    }
+}
