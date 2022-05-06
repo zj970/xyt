37d9669061996ce1d9177f1571916c7c29de7826
zj970 <3060529292@qq.com>
2022-05-06 22:03:17 +0800

    毕业设计-校园通-2022-5-5 添加redis配置文件，student显示信息有误，添加adminController

---
+++ GitHubWarehouse/xyt/src/main/java/com/zj/xyt/Controller/admin/AdminController.java
@@ -0,0 +1,15 @@
+package com.zj.xyt.Controller.admin;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author zj
+ * @since 2022/3/12 15:00
+ */
+@Controller
+@RequestMapping("/admin")
+@Api(tags = "管理员接口")
+public class AdminController {
+}
